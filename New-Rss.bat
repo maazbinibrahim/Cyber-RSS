9fb1ec6626ce2d19fa7f4db365c2742d55d568b5
Maaz Bin Ibrahim <60464565+maazbinibrahim@users.noreply.github.com>
2024-10-22 17:54:55 +0500

    Add files via upload

---
+++ New-Rss.bat
@@ -0,0 +1,8 @@
+@echo off
+@echo on "RSS Reader Script Start"
+cd "C:\Users\hp\Reader-RSS"
+"C:/Users/hp/AppData/Local/Microsoft/WindowsApps/python3.12.exe" "C:\Users\hp\Reader-RSS\seven.py"
+@echo on "Go To Desktop To See Results"
+@echo on " Script Compelted Successfully you can hit any button to close the window"
+start "" "C:\Users\hp\Reader-RSS"
+pause
