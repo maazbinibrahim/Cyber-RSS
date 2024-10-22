791bc89c10d3b3be52e72fabc6917c96522906d3
Maaz Bin Ibrahim <60464565+maazbinibrahim@users.noreply.github.com>
2024-10-22 18:37:47 +0500

    Update New-Rss.bat

--- New-Rss.bat
+++ New-Rss.bat
@@ -1,8 +1,8 @@
 @echo off
 @echo on "RSS Reader Script Start"
-cd "C:\Cyber-RSS"
-"python" "C:\Cyber-RSS\Launch.py"
-@echo on "Go To Cyber-RSS Folder To See Results"
+cd "C:\Cyber-RSS-main"
+"python" "C:\Cyber-RSS-main\Launch.py"
+@echo on "Go To Cyber-RSS-main Folder To See Results"
 @echo on " Script Completed Successfully you can hit any button to close the window"
-start "" "C:\Cyber-RSS"
+start "" "C:\Cyber-RSS-main"
 pause
