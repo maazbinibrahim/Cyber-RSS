167f5531fe6aa737c6cea572310ea9e5ef9c2d9a
Maaz Bin Ibrahim <60464565+maazbinibrahim@users.noreply.github.com>
2024-10-22 18:19:54 +0500

    Update New-Rss.bat

--- New-Rss.bat
+++ New-Rss.bat
@@ -1,8 +1,8 @@
 @echo off
 @echo on "RSS Reader Script Start"
-cd "C:\Users\hp\Reader-RSS"
-"C:/Users/hp/AppData/Local/Microsoft/WindowsApps/python3.12.exe" "C:\Users\hp\Reader-RSS\seven.py"
+cd "C:\Reader-RSS"
+"C:/Users/hp/AppData/Local/Microsoft/WindowsApps/python3.12.exe" "C:\Reader-RSS\Launch.py"
 @echo on "Go To Desktop To See Results"
 @echo on " Script Compelted Successfully you can hit any button to close the window"
-start "" "C:\Users\hp\Reader-RSS"
+start "" "C:\Reader-RSS"
 pause
